0c3b62e8a38deb51596428b2d666b3dc5626132b
thlux <thlux@mail.roanoke.edu>
2014-06-05 09:37:27 -0400

    Woring on OpenCV, got the first video to play with the VideoCapture class.

---
+++ OpenCV/CMakeFiles/firstProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/firstProgram.dir/firstProgram.cc.o"
+  "firstProgram.pdb"
+  "firstProgram"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/firstProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- OpenCV/firstProgram.cc
+++ OpenCV/firstProgram.cc
@@ -1,22 +1,38 @@
-// For macros and initialization protocols
-//#include "cxtypes.h"
-#include "cxmisc.h"
-// For compiling computer vision related code
-#include "cv.h"
-#include "cxcore.h"
-// For compiling machine learning related code
-#include "highgui.h"
-#include "ml.h"
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
 
-using namespace std;
+using namespace cv;
 
-// This program is directly from the OpenCV textbook
-int main(int argc, char * argv[]){
-  IplImage* img = cvLoadImage( argv[1] );
-  cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
-  cvShowImage( "Example1", img );
-  cvWaitKey(0);
-  cvReleaseImage( &img );
-  cvDestroyWindow( "Example1" );
-  return(0);
+// This program will display the image passed as a commandline arg
+int main(int argc, char** argv ){
+  int status = 0; // Run status of program
+
+  if ( argc != 2 )    {
+    printf("usage: DisplayImage.out <Image_Path>\n");
+    status = -1;
+  }
+  Mat image; // http://docs.opencv.org/modules/core/doc/basic_structures.html#mat
+  image = imread( argv[1], 1 );
+  // http://docs.opencv.org/modules/highgui/doc/reading_and_writing_images_and_video.html#Mat%20imread%28const%20string&%20filename,%20int%20flags%29
+
+  if ( !image.data ){
+    printf("No image data \n");
+    status = -1;
+  }
+
+  namedWindow("Display Image", CV_WINDOW_AUTOSIZE ); // Creates a
+  //  window that the user can place an image in that can be referred
+  //  to by the string parameter given
+  imshow("Display Image", image);
+  // The first paramater is the name of the window to put the image
+  //  in, the second is the matrix for the image to be loaded into the
+  //  window 
+  waitKey(0);
+  // Waits for a pressed key if = 0, otherwise, waits until the given
+  //  number of miliseconds has ellapsed
+
+  // http://docs.opencv.org/modules/highgui/doc/user_interface.html
+  // ^^ Has all three of the above function calls
+
+  return(status);
 }
